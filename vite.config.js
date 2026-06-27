40311fb637b5f53787693ced71e6403a736fc128
bikram73 <bikram8548@gmail.com>
2026-06-27 10:29:17 +0530

    first commit

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,8 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
+// https://vitejs.dev/config/
 export default defineConfig({
+  base: '/Task_Buddy/',
   plugins: [react()],
-  base: '/TaskMan/', // must match your repo name
 })
